7413c3753dfd9fb6fff285dce7c1db0600513889
S4chan <heineken1204@gmail.com>
2024-12-13 15:11:22 +0800

    git init

---
+++ src/components/Main.js
@@ -0,0 +1,39 @@
+import "../styles/main.scss";
+import StepProgress from "./StepProgress";
+import StepOne from "./StepOne";
+import StepTwo from "./StepTwo";
+import StepThree from "./StepThree";
+import ProgressControl from "./ProgressControl";
+
+export default function Main() {
+  return (
+    <>
+      <main className="site-main">
+        <div className="main-container">
+          {/* register */}
+          <section
+            className="register-container col col-lg-6 col-sm-12"
+            data-phase={1}
+            data-total-price={0}
+          >
+            {/* register-title */}
+            <h2 className="register-title col col-12">結帳</h2>
+            {/* register-progress */}
+            <StepProgress />
+            {/* register-form */}
+            <section className="form-container col col-12">
+              {/* address phase */}
+              <StepOne />
+              {/* shipping phase */}
+              <StepTwo />
+              {/* credit-card phase */}
+              <StepThree />
+            </section>
+          </section>
+          {/* progress-control */}
+          <ProgressControl />
+        </div>
+      </main>
+    </>
+  );
+}
